59219a7f5eafca27a427db65c4950cb5ce712133
uiwow7 <Voyager.CustomMTG@gmail.com>
2025-08-25 09:34:42 -0700

    sddd

--- buildhub.py
+++ buildhub.py
@@ -1,7 +1,17 @@
 import os, shutil
 
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            # print("copying", s, d)
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
 try:
-	os.rmdir("export")
+	shutil.rmtree("export")
 except:
 	print("Couldn't remove export")
 
@@ -20,9 +30,17 @@ for set in listed_dir:
 print("Moving to github repo")
 
 try:
-	shutil.copytree("export", "../uiwow7.github.io/sets")
+	shutil.rmtree("../uiwow7.github.io/sets")
+except:
+	print("Couldn't remove from sets from uiwow7.github.io")
+
+try:
+	copytree("export", "../uiwow7.github.io/sets")
 except:
 	print("Couldn't move from export > uiwow7.github.io")
+
+os.mkdir("../uiwow7.github.io/sets/versions")
+os.mkdir("../uiwow7.github.io/sets/versions/changelogs")
 
 os.chdir("../uiwow7.github.io")
 
